841513c0a5e7749c1610a8741954f2ff80fc0697
Richard Cândido <rich_dacan@protonmail.com>
2023-06-13 19:22:15 -0300

    fix :bug: fixedd bug in next

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: [
+      "s3.amazonaws.com",
+      "images.prismic.io",
+      "img.youtube.com",
+      "lh3.googleusercontent.com",
+      "avatars.githubusercontent.com",
+      "raw.githubusercontent.com",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,13 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import { useSession, signIn, signOut } from "next-auth/react";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const { data: session } = useSession();
 
-  // console.log("session");
+  console.log(session);
 
   if (!session) {
     return (
@@ -16,7 +16,7 @@ export default function Home() {
           Not signed in <br />
           <button
             onClick={() => signIn("google")}
-            className="bg-gray-600 p-2 rounded-lg "
+            className="bg-gray-600 p-2 rounded-lg"
           >
             Login with Google
           </button>
@@ -26,9 +26,24 @@ export default function Home() {
   }
 
   return (
-    <div className="bg-slate-950">
-      Signed in as {session?.user?.email} <br />
-      <button onClick={() => signOut()}>Sign out</button>
+    <div className="bg-slate-950 w-screen h-screen flex-col">
+      <h3>
+        {" "}
+        Signed in as {session?.user?.email} <br />
+      </h3>
+      {session && (
+        <Image
+          src={session?.user?.image ? session?.user?.image : ""}
+          width={250}
+          height={250}
+          alt={`profile picture of the user ${session?.user?.name}`}
+          priority
+          quality={100}
+        />
+      )}
+      <button className="bg-gray-600 p-2 rounded-lg" onClick={() => signOut()}>
+        Sign out
+      </button>
     </div>
   );
 }
