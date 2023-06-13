79d99483b1cb6eb86978b3b89a5367f513f4c985
Richard Cândido <rich_dacan@protonmail.com>
2023-06-13 13:23:21 -0300

    fwat :sparkles: added next auth and login with google

---
+++ src/pages/api/auth/[...nextauth].tsx
@@ -0,0 +1,29 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+// import AppleProvider from 'next-auth/providers/apple'
+// import FacebookProvider from 'next-auth/providers/facebook'
+// import EmailProvider from 'next-auth/providers/email'
+
+export default NextAuth({
+  providers: [
+    // OAuth authentication providers...
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+    // AppleProvider({
+    //   clientId: process.env.APPLE_ID,
+    //   clientSecret: process.env.APPLE_SECRET
+    // }),
+    // FacebookProvider({
+    //   clientId: process.env.FACEBOOK_ID,
+    //   clientSecret: process.env.FACEBOOK_SECRET
+    // }),
+    // Passwordless / email sign in
+    //   EmailProvider({
+    //     server: process.env.MAIL_SERVER,
+    //     from: 'NextAuth.js <no-reply@example.com>'
+    //   }),
+  ],
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,30 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <div className="bg-red-500">Renewals</div>;
+  const { data: session } = useSession();
+  if (session) {
+    return (
+      <>
+        Signed in as {session?.user?.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    );
+  }
+  return (
+    <div className="bg-slate-900 w-screen h-screen flex items-center">
+      <div className="text-center w-full">
+        Not signed in <br />
+        <button
+          onClick={() => signIn("google")}
+          className="bg-gray-600 p-2 rounded-lg "
+        >
+          Login with Google
+        </button>
+      </div>
+    </div>
+  );
 }
