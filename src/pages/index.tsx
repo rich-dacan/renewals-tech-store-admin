13a6257eb134682f5ae717056c0233a091cf32b4
Richard Cândido <rich_dacan@protonmail.com>
2023-06-13 20:08:10 -0300

    feat :sparkles: created navbar component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import { useSession, signIn, signOut } from "next-auth/react";
 import Image from "next/image";
+import { NavbarAside } from "@/components/NavbarAside";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,24 +27,30 @@ export default function Home() {
   }
 
   return (
-    <div className="bg-slate-950 w-screen h-screen flex-col">
-      <h3>
-        {" "}
-        Signed in as {session?.user?.email} <br />
-      </h3>
-      {session && (
-        <Image
-          src={session?.user?.image ? session?.user?.image : ""}
-          width={250}
-          height={250}
-          alt={`profile picture of the user ${session?.user?.name}`}
-          priority
-          quality={100}
-        />
-      )}
-      <button className="bg-gray-600 p-2 rounded-lg" onClick={() => signOut()}>
-        Sign out
-      </button>
+    <div className="flex min-h-screen">
+      <NavbarAside />
+      <div className="bg-white h-screen flex flex-grow p-4 mt-2 mb-2 mr-2 rounded-lg">
+        <p className="pb-1 text-black"> Hello, {session?.user?.name}!</p>
+
+        {session && (
+          <Image
+            src={session?.user?.image ? session?.user?.image : ""}
+            width={50}
+            height={50}
+            alt={`profile picture of the user ${session?.user?.name}`}
+            priority
+            quality={100}
+            className="h-10 ml-2 rounded-lg"
+          />
+        )}
+
+        <button
+          className="bg-gray-600 p-2 rounded-lg h-10 ml-2"
+          onClick={() => signOut()}
+        >
+          Sign out
+        </button>
+      </div>
     </div>
   );
 }
